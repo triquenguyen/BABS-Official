fc4dfc780745850239111673b7509e507274d9f0
Trique Nguyen <ngngoctridung123@gmail.com>
2023-04-17 00:07:09 -0700

    working on dashboard

--- my-babs-app/pages/dashboard.tsx
+++ my-babs-app/pages/dashboard.tsx
@@ -4,18 +4,48 @@ import Image from 'next/image';
 import DepositCheck from './components/DepositCheck';
 import { GetServerSideProps } from 'next';
 import { prisma } from '../libs/prisma';
+import { useEffect, useState } from 'react';
 
-export default function dashboard({ balance, totalDeposits, totalWithdrawals }) {
+interface DashboardProps {
+  balance: number;
+  totalDeposits: number;
+  totalWithdrawals: number;
+}
+
+
+
+export default function dashboard() {
   const { status, data: session } = useSession();
+  const [dashboard, setDashboard] = useState<DashboardProps>({ balance: 0, totalDeposits: 0, totalWithdrawals: 0 });
 
   const handleSignout = () => {
     signOut({ redirect: true, callbackUrl: '/login' })
   }
 
-
   if (!session) {
     return <h1>you gotta log in {status}</h1>;
   }
+
+  useEffect(() => {
+    const fetchUser = async () => {
+
+      const user = await prisma.user.findUnique({
+        where: {
+          id: session.user?.id
+        }, select: {
+          balance: true,
+          totalDeposit: true,
+          totalWithdraw: true
+        }
+      })
+
+      setDashboard({
+        balance: Number (user?.balance),
+        totalDeposits: Number (user?.totalDeposit) ,
+        totalWithdrawals: Number (user?.totalWithdraw)
+      })
+    }
+  }, [])
 
   return (
     <div>
@@ -23,43 +53,43 @@ export default function dashboard({ balance, totalDeposits, totalWithdrawals }) 
       <div className="px-8 py-4">
         <Navbar username={session.user?.name} />
         <DepositCheck id={session.user?.id} />
-        <h1>Balance {balance}</h1>
-        <h1>Total Deposits {totalDeposits}</h1>
-        <h1>Total Withdrawals {totalWithdrawals}</h1>
+        <h1>Balance {dashboard.balance}</h1>
+        <h1>Total Deposits {dashboard.totalDeposits}</h1>
+        <h1>Total Withdrawals {dashboard.totalWithdrawals}</h1>
         <button className="px-3 py-2 bg-[#69C9D0] bg-opacity-70 rounded-md text-white" onClick={handleSignout}>Sign Out</button>
       </div>
     </div>
   )
 }
 
-export async function getServerSideProps(context) {
-  const session = await getSession(context);
+// export async function getServerSideProps(context) {
+//   const session = await getSession(context);
 
-  if (session) {
-    const user = await prisma.user.findUnique({
-      where: {
-        id: session?.user?.id
-      }, select: {
-        balance: true,
-        totalDeposit: true,
-        totalWithdraw: true
-      }
-    })
+//   if (session) {
+//     const user = await prisma.user.findUnique({
+//       where: {
+//         id: session?.user?.id
+//       }, select: {
+//         balance: true,
+//         totalDeposit: true,
+//         totalWithdraw: true
+//       }
+//     })
 
-    return {
-      props: {
-        balance: user?.balance,
-        totalDeposits: user?.totalDeposit,
-        totalWithdrawals: user?.totalWithdraw
-      }
-    }
-  } else {
-    return {
-      props: {
-        balance: 0,
-        totalDeposits: 0,
-        totalWithdrawals: 0
-      }
-    }
-  }
-}
+//     return {
+//       props: {
+//         balance: user?.balance,
+//         totalDeposits: user?.totalDeposit,
+//         totalWithdrawals: user?.totalWithdraw
+//       }
+//     }
+//   } else {
+//     return {
+//       props: {
+//         balance: 0,
+//         totalDeposits: 0,
+//         totalWithdrawals: 0
+//       }
+//     }
+//   }
+// }
