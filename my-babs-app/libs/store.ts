b1225f5c3aa21bff0345000b35fb71a9452038af
Trique Nguyen <ngngoctridung123@gmail.com>
2023-04-27 17:36:12 -0700

    fixed reducer in store

--- my-babs-app/libs/store.ts
+++ my-babs-app/libs/store.ts
@@ -2,14 +2,14 @@ import { configureStore } from '@reduxjs/toolkit'
 import showReducer from '../pages/redux/showSlice'
 import showTransferReducer from '../pages/redux/showTransferSlice'
 import showWithdrawReducer from '@/pages/redux/showWithdrawSlice'
-import showTransactionSlice from '@/pages/redux/showTransactionSlice'
+import showTransactionReducer from '@/pages/redux/showTransactionSlice'
 
 export const store = configureStore({
   reducer: {
     show: showReducer,
     showTransfer: showTransferReducer,
     showWithdraw: showWithdrawReducer,
-    showTransaction: showTransactionSlice,
+    showTransaction: showTransactionReducer,
   },
 })
 
