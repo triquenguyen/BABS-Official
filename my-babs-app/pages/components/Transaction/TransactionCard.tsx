2cd0862b886e6db56c15af8d0b77b8f163ac82d9
Giao Dinh <114348542+giaodinhsjsu@users.noreply.github.com>
2023-05-02 11:45:55 +0700

    add formater

--- my-babs-app/pages/components/Transaction/TransactionCard.tsx
+++ my-babs-app/pages/components/Transaction/TransactionCard.tsx
@@ -6,16 +6,17 @@ const plexMono = IBM_Plex_Mono({
 })
 
 export default function TransactionCard({ type, amount, id, date, balanceBefore, balanceAfter, receiver, sender }) {
+  const formater = new Intl.NumberFormat("en-us", {style: 'decimal'});
   if (type == "Transfer" && receiver != null) {
     return (
-      <div className={`flex  flex-col text-white bg-[#69C9D0] p-4 gap-2 items-center justify-center rounded-md`}>
+      <div className={`flex flex-col text-white bg-[#69C9D0] p-4 gap-2 items-center justify-center rounded-md`}>
         <div className="flex gap-6">
           <h1>{type}</h1>
           <h1>{id}</h1>
           <h1 className={`${plexMono.className}`}>$ {amount}</h1>
           <h1>{date}</h1>
-          <h1 className={`${plexMono.className}`}>$ {balanceBefore}</h1>
-          <h1 className={`${plexMono.className}`}>$ {balanceAfter}</h1>
+          <h1 className={`${plexMono.className}`}>$ {formater.format(balanceBefore)}</h1>
+          <h1 className={`${plexMono.className}`}>$ {formater.format(balanceAfter)}</h1>
         </div>
 
         <h1>Send to {receiver}</h1>
@@ -23,14 +24,14 @@ export default function TransactionCard({ type, amount, id, date, balanceBefore,
     )
   } else if (type == "Transfer" && sender != null) {
     return (
-      <div className={`flex  flex-col text-white bg-[#69C9D0] p-4 gap-2 items-center justify-center rounded-md`}>
+      <div className={`flex flex-col text-white bg-[#69C9D0] p-4 gap-2 items-center justify-center rounded-md`}>
         <div className="flex gap-6">
           <h1>{type}</h1>
           <h1>{id}</h1>
           <h1 className={`${plexMono.className}`}>$ {amount}</h1>
           <h1>{date}</h1>
-          <h1 className={`${plexMono.className}`}>$ {balanceBefore}</h1>
-          <h1 className={`${plexMono.className}`}>$ {balanceAfter}</h1>
+          <h1 className={`${plexMono.className}`}>$ {formater.format(balanceBefore)}</h1>
+          <h1 className={`${plexMono.className}`}>$ {formater.format(balanceAfter)}</h1>
         </div>
 
         <h1>Send from {sender}</h1>
@@ -43,8 +44,8 @@ export default function TransactionCard({ type, amount, id, date, balanceBefore,
         <h1>{id}</h1>
         <h1 className={`${plexMono.className}`}>$ {amount}</h1>
         <h1>{date}</h1>
-        <h1 className={`${plexMono.className}`}>$ {balanceBefore}</h1>
-        <h1 className={`${plexMono.className}`}>$ {balanceAfter}</h1>
+        <h1 className={`${plexMono.className}`}>$ {formater.format(balanceBefore)}</h1>
+        <h1 className={`${plexMono.className}`}>$ {formater.format(balanceAfter)}</h1>
       </div>
     )
   }
